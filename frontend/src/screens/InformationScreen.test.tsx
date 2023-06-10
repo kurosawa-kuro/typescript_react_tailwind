378ba7ef78cac6e1f6c0646a8dad71b9e60255b3
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-06-11 00:21:52 +0900

    fix

--- frontend/src/screens/InformationScreen.test.tsx
+++ frontend/src/screens/InformationScreen.test.tsx
@@ -1,8 +1,24 @@
 // src/screens/InformationScreen.test.tsx
 
 import { render, screen, waitFor } from "@testing-library/react";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
 import { InformationScreen } from "./InformationScreen";
-import { server } from "../mocks/handlers";
+
+const handlers = [
+  rest.get("https://jsonplaceholder.typicode.com/users", (_req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      ctx.json([
+        { id: 1, name: "John" },
+        { id: 2, name: "Doe" },
+        // Add more mock data if necessary
+      ])
+    );
+  }),
+];
+
+const server = setupServer(...handlers);
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
